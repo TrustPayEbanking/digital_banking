98cfa0ac0eedbb76b8cad923f5aaabe071281d0a
hamdane1548 <oussamahamdane80@gmail.com>
2025-12-28 16:13:48 +0100

    attribu entites customer and bankeccount

--- src/main/java/net/oussama/ebankingbackend/entites/BankAccount.java
+++ src/main/java/net/oussama/ebankingbackend/entites/BankAccount.java
@@ -1,4 +1,12 @@
 package net.oussama.ebankingbackend.entites;
 
+import net.oussama.ebankingbackend.enums.AccountStatus;
+
+import java.util.Date;
+
 public class BankAccount {
+    private String id;
+    private double balance;
+    private Date date;
+    private AccountStatus status;
 }
--- src/main/java/net/oussama/ebankingbackend/entites/Customer.java
+++ src/main/java/net/oussama/ebankingbackend/entites/Customer.java
@@ -1,4 +1,18 @@
 package net.oussama.ebankingbackend.entites;
 
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+@Data
+@AllArgsConstructor @NoArgsConstructor
 public class Customer {
+    @Id
+    private Long Id;
+    private String name;
+    private String LastName;
+    private String Email;
+    private List<BankAccount> BankAccounts;
 }
